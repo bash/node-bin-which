ab4c551521753ff19dbf1838ee1d8c88764a8a10
Ruby Schmidmeister <ruben.schmidmeister@icloud.com>
2016-01-08 10:46:21 +0100

    add new test

--- test/test-which.js
+++ test/test-which.js
@@ -4,16 +4,23 @@ var assert = require('assert'),
     which = require('../lib/which'),
     path = require('path');
 
-describe('which()', function(){
-    it('returns an array', function(){
+describe('which()', function () {
+    it('returns an array', function () {
         assert(which('', { HOME: '/', PWD: '/', PATH: '/' }) instanceof Array);
     });
 
-    it('looks up the absolute path of the executable', function(){
-        var dir = path.resolve(__dirname +'/../node_modules/.bin'),
+    it('looks up the absolute path of the executable', function () {
+        var dir = path.resolve(__dirname + '/../node_modules/.bin'),
             env = { HOME: '/', PWD: '/', PATH: dir };
 
         assert.deepEqual([dir + '/mocha'], which('mocha', env));
         assert.deepEqual([dir + '/istanbul'], which('istanbul', env));
+    });
+
+    it('returns an empty array if nothing is found', function () {
+        var dir = path.resolve(__dirname + '/../node_modules/.bin'),
+            env = { HOME: '/', PWD: '/', PATH: dir };
+
+        assert.deepEqual([], which('this-does-not-exists', env));
     });
 });
